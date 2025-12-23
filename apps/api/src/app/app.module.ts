49610a3f42bba0e9ab4ab96365fd64dae9462026
Ankush Kumar <ankush.bzrms@gmail.com>
2025-12-23 15:14:31 +0530

    nest api setup done

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+
+// nx g @nx/nest:application --name=api --directory=apps/api
+
+
+}
